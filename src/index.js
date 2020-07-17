dd96654acc5242678c5df33e4e3acd788e2ce9db
jamieberrier <berrier.jamie@gmail.com>
2020-07-17 14:54:11 -0700

    add DOMContentLoaded event listener

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 console.log("in index.js");
+
+const endPoint = "http://localhost:3000/api/v1/posts"
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('LOADED');
+    fetch(endPoint)
+    .then(response => response.json())
+    .then(posts => {
+      console.log(posts);
+    })
+  });
