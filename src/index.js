5174f1bc347d7aa29ca28065979ebec10b3ec167
jamieberrier <berrier.jamie@gmail.com>
2020-07-17 15:13:36 -0700

    create getPosts function to fetch posts and display them in the DOM

--- src/index.js
+++ src/index.js
@@ -4,9 +4,24 @@ const endPoint = "http://localhost:3000/api/v1/posts"
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log('LOADED');
-    fetch(endPoint)
-    .then(response => response.json())
-    .then(posts => {
-      console.log(posts);
-    })
+    getPosts()
   });
+
+/* GET request */
+function getPosts() {
+  fetch(endPoint)
+  .then(res => res.json())
+  .then(posts => {
+    console.log(posts)
+    posts.data.forEach(post => {
+      const postMarkup = `
+      <div data-id=${post.id}>
+        <img src=${post.attributes.picture} height="300" width="350">
+        <h3>Breed: ${post.attributes.breed.name}</h3>
+        <p>Likes: ${post.attributes.num_of_likes}</p>
+      </div>
+      <br><br>`;
+      document.querySelector('#post-container').innerHTML += postMarkup
+    })
+  })
+}
