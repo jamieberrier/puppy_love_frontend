2aae6555fbd4d47d8e4e8353ab830436ea30437f
jamieberrier <berrier.jamie@gmail.com>
2020-07-20 14:33:47 -0700

    create function likePost() to handle click event on heart and wantOne() to handle click event on 'I want One!'

--- src/index.js
+++ src/index.js
@@ -21,3 +21,30 @@ function getPosts() {
     })
   });
 }
+
+function likePost(event) {
+  const heart = event.target
+
+  if (heart.innerText == EMPTY_HEART) {
+    // Change the heart to a full heart
+    heart.innerText = FULL_HEART
+    // Add the .activated-heart class to make the heart appear red
+    heart.setAttribute("class", "activated-heart")
+    // increase post num_of_likes
+  } else { // When a user clicks on a full heart
+    // Change the heart back to an empty heart
+    heart.innerText = EMPTY_HEART
+    // Remove the .activated-heart class
+    heart.removeAttribute("class", "activated-heart")
+    // decrease post num_of_likes
+  }
+}
+
+function wantDog(event) {
+  const dogBreedId = event.target.dataset.id
+  const box = document.getElementById(`${dogBreedId}`)
+  const adoptContainer = document.createElement("div")
+  adoptContainer.innerText = "Dogs Available for Adoption"
+  // fetch available dogs from rescuegroups.org api
+  box.appendChild(adoptContainer)
+}
