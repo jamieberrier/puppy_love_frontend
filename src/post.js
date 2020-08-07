1f233ea43ab51513d9d81c41981a73c682509d91
jamieberrier <berrier.jamie@gmail.com>
2020-08-07 13:02:38 -0700

    refactor fetchAdoptableDogs and renderAdoptableDog to instance methods

--- src/post.js
+++ src/post.js
@@ -86,7 +86,7 @@ class Post {
       body: JSON.stringify(bodyData)
     };
   
-    return fetch(`${postsEndPoint}/${this.id}`, configObj)
+    fetch(`${postsEndPoint}/${this.id}`, configObj)
     .then(response => response.json())
     .then(post => {
       const likes = post.data.attributes.num_of_likes
@@ -98,6 +98,126 @@ class Post {
       p.num_of_likes = likes
     })
     .catch(error => console.log(error.message))
+  }
+
+  // GET request - adoptale dogs of the same breed as the post from petfinder API
+  fetchAdoptableDogs(breed, token) {
+    const wantOne = document.querySelector(`#want-one-${this.id}`)
+    const page = document.querySelector("html")
+    const box = document.querySelector(`#box-${this.id}`)
+    // create container for adoptable dogs
+    const adoptContainer = document.createElement("article")
+    adoptContainer.setAttribute("id", `adoption-container-${this.id}`)
+    adoptContainer.setAttribute("class", "message is-danger")
+    // create header
+    const adoptHeaderDiv = document.createElement("div")
+    adoptHeaderDiv.setAttribute("class", "message-header heading is-size-5")
+    const adoptHeader = document.createElement("p")
+    adoptHeaderDiv.appendChild(adoptHeader)
+    // create close button
+    const closeBtn = document.createElement("button")
+    closeBtn.setAttribute("class", "delete")
+    closeBtn.setAttribute("aria-label", "delete")
+    // add listener to close button
+    closeBtn.addEventListener("click", handleCloseAdoptionContainer)
+    adoptHeaderDiv.appendChild(closeBtn)
+    adoptContainer.appendChild(adoptHeaderDiv)
+    // pluralize breed
+    const breedPlural = pluralize(breed)
+
+    let configObj = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Authorization": `Bearer ${token}`
+      }
+    };
+    
+    fetch (`https://api.petfinder.com/v2/animals?type=dog&breed=${breed}&status=adoptable&limit=100`, configObj)
+    .then(response => response.json())
+    .then(dogs => {
+      // change back text color & cursor
+      wantOne.setAttribute("class", "has-text-danger level-item like")
+      page.style.cursor = "auto"
+      // if adoptable dog(s) found
+      if (!!dogs.animals) {
+        // set header
+        adoptHeader.innerText = `Adoptable ${breedPlural}`
+        // add adoption div to post
+        box.appendChild(adoptContainer)
+        // get org info then render each adoptable dog
+        dogs.animals.forEach(dog => {
+          //renderAdoptableDog(dog, this.id)
+          this.renderAdoptableDog(dog)
+        })
+      } else { // no adoptable dog(s) found
+        // set header
+        adoptHeader.innerText = ` No Adoptable ${breedPlural}`
+        // add adoption div to post
+        box.appendChild(adoptContainer)
+      }
+    })
+    .catch(error => {
+      console.log(error.message)
+      // set header
+      adoptHeader.innerText = ` No Adoptable ${breedPlural}`
+      // add adoption div to post
+      box.appendChild(adoptContainer)
+    })
+  }
+
+  // Generating HTML for each adoptable dog and appending to adoptContainer
+  renderAdoptableDog(dog) {
+    const adoptContainer = document.querySelector(`#adoption-container-${this.id}`)
+    const dogDiv = document.createElement("article")
+    dogDiv.setAttribute("class", "media")
+    // media-left
+    const figure = document.createElement("figure");
+    figure.setAttribute("class", "media-left mt-4 mb-4")
+    // <p> for picture
+    const picP = document.createElement("p")
+    picP.setAttribute("class", "image is-96x96")
+    const pic = document.createElement("img")
+    // if has primary photo
+    if (dog.primary_photo_cropped != null) {
+      pic.src = dog.primary_photo_cropped.medium
+    } else {
+      pic.src = "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/48637918/1/?bust=1596122132&width=450"
+    }
+    picP.appendChild(pic)
+    figure.appendChild(picP)
+    dogDiv.appendChild(figure)
+    // media-content
+    const mediaContent = document.createElement("div")
+    mediaContent.setAttribute("class", "media-content")
+    const content = document.createElement("div")
+    content.setAttribute("class", "content")
+    // <p> for name
+    const name = document.createElement("p")
+    name.setAttribute("class", "has-text-danger is-size-5 mb-1")
+    name.innerText = dog.name
+    content.appendChild(name)
+    // <p> for details
+    const details = document.createElement("p")
+    details.innerHTML = `${dog.age} ` + '<i class="fas fa-paw"></i>' + ` ${dog.gender} ` + '<i class="fas fa-paw"></i>' + ` ${dog.size}`
+    content.appendChild(details)
+    // <p> for location
+    const location = document.createElement("p")
+    location.innerHTML = '<i class="fas fa-map-marked-alt"></i>' + ` ${dog.contact.address.city}` + ', ' + `${dog.contact.address.state}`
+    content.appendChild(location)
+    // <p> for url
+    const urlP = document.createElement("p")
+    urlP.setAttribute("class", "has-text-link")
+    // <a> for url
+    const url = document.createElement("a")
+    url.setAttribute("href", `${dog.url}`)
+    url.setAttribute("target", "_blank")
+    url.innerText = `Tell Me More About ${dog.name}!`
+    urlP.appendChild(url)
+    content.appendChild(urlP)
+    mediaContent.appendChild(content)
+    dogDiv.appendChild(mediaContent)
+    adoptContainer.appendChild(dogDiv)
   }
 }
 
