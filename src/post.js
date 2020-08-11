b7f15281894f3a4f53957c987ac2b637cbaf1339
jamieberrier <berrier.jamie@gmail.com>
2020-08-11 13:41:26 -0700

    add and update comments

--- src/post.js
+++ src/post.js
@@ -8,6 +8,7 @@ class Post {
     Post.all.push(this);
   }
 
+  // Generating HTML for a post
   renderPost() {
     const postsContainer = document.querySelector("#posts-container");
     // create box
@@ -15,49 +16,43 @@ class Post {
     postBox.setAttribute("class", "box");
     postBox.setAttribute("id", `box-${this.id}`);
     postBox.setAttribute("data-breed-id", `${this.breed.id}`)
-    // picture
+    // create picture
     const figure = document.createElement("figure");
     figure.setAttribute("class", "image is-256x256")
     const postImage = document.createElement("img");
     postImage.src = this.picture;
     figure.appendChild(postImage);
     postBox.appendChild(figure);
-    // create level for Likes & I Want One
+    // create level - Likes & I Want One
     const level = document.createElement("nav");
     level.setAttribute("class", "level is-mobile mt-2");
     level.setAttribute("id", "level")
-    // left
+    // create left level
     const left = document.createElement("div");
     left.setAttribute("class", "level-left is-size-5")
-    // heart
-    //<i class="far fa-heart"></i>
-    //const heart = document.createElement("i");
+    //  create heart
     const heart = document.createElement("span");
-    //heart.setAttribute("class", "level-item far fa-heart")
     heart.setAttribute("class", "like-glyph like")
     heart.setAttribute("data-id", this.id)
     heart.innerHTML = '&#x2661';
     heart.addEventListener("click", likePost)
     left.appendChild(heart)
-    // likes
+    // create number of likes
     const postLikes = document.createElement('p');
     postLikes.setAttribute("class", "level-item")
     postLikes.innerText = this.num_of_likes;
     left.appendChild(postLikes);
     level.appendChild(left);
-    // right
+    // create right level
     const right = document.createElement("div");
     right.setAttribute("class", "level-right")
-    // I Want One
-    // <i class="fas fa-dog"></i>
+    // create 'I Want One' button
     const wantOne = document.createElement("button");
     wantOne.setAttribute("class", "level-item button is-danger is-light is-rounded")
     wantOne.setAttribute("id", `want-one-${this.id}`)
     wantOne.setAttribute("data-post-id", this.id)
     wantOne.setAttribute("data-breed", this.breed.name)
-    // <i class="far fa-grin-hearts"></i>
     wantOne.innerText = 'I Want One!'
-    //wantOne.style = "text-decoration: underline"
     wantOne.addEventListener("click", wantDog)
     right.appendChild(wantOne);
     level.appendChild(right);
@@ -69,10 +64,13 @@ class Post {
     return postBox
   }
 
+  // Finding post by id
   static findById(id) {
     return this.all.find(post => post.id === id);
   }
 
+  // Updating a post's number of likes
+  // // PATCH request
   updateLikes(num_of_likes) {
     let bodyData = {
       id: this.id,
@@ -91,8 +89,11 @@ class Post {
     fetch(`${POSTS_END_POINT}/${this.id}`, configObj)
     .then(response => response.json())
     .then(post => {
+      // get number of likes
       const likes = post.data.attributes.num_of_likes
+      // get post
       const box = document.querySelector(`#box-${post.data.id}`)
+      // update DOM with new number of likes
       box.children[1].firstElementChild.children[1].innerText = likes
       // update num_of_likes in Post.all
       const postId = parseInt(post.data.id)
@@ -102,7 +103,8 @@ class Post {
     .catch(error => console.log(error.message))
   }
 
-  // GET request - adoptale dogs of the same breed as the post from petfinder API
+  // Fetching adoptale dogs of the same breed as the post from petfinder API
+  // // GET request
   fetchAdoptableDogs(breed, token) {
     const wantOne = document.querySelector(`#want-one-${this.id}`)
     const page = document.querySelector("html")
@@ -123,7 +125,7 @@ class Post {
     .then(dogs => {
       // hide 'I Want One!' button
       wantOne.setAttribute("class", "is-hidden")
-      // change back cursor
+      // change cursor back
       page.style.cursor = "auto"
       // if adoptable dog(s) found
       if (dogs.animals.length > 0) {
@@ -131,10 +133,12 @@ class Post {
         const adoptContainer = document.createElement("article")
         adoptContainer.setAttribute("id", `adoption-container-${this.id}`)
         adoptContainer.setAttribute("class", "message is-danger")
-        // create header
+        // create header <div>
         const adoptHeaderDiv = document.createElement("div")
         adoptHeaderDiv.setAttribute("class", "message-header heading is-size-5")
+        // create header <p>
         const adoptHeader = document.createElement("p")
+        adoptHeader.innerText = `Adoptable ${breedPlural}`
         adoptHeaderDiv.appendChild(adoptHeader)
         // create close button
         const closeBtn = document.createElement("button")
@@ -142,20 +146,17 @@ class Post {
         closeBtn.setAttribute("aria-label", "delete")
         // add listener to close button
         closeBtn.addEventListener("click", (e) => {
-          // remove adoptable dogs
+          // remove adoptable dogs container
           adoptContainer.remove()
-          // bring back 'I Want One!' button
+          // show 'I Want One!' button
           wantOne.setAttribute("class", "level-item button is-danger is-light is-rounded")
         })
         adoptHeaderDiv.appendChild(closeBtn)
         adoptContainer.appendChild(adoptHeaderDiv)
-        // set header
-        adoptHeader.innerText = `Adoptable ${breedPlural}`
-        // add adoption div to post
+        // add adoption container to post
         box.appendChild(adoptContainer)
-        // get org info then render each adoptable dog
+        // render each adoptable dog
         dogs.animals.forEach(dog => {
-          //renderAdoptableDog(dog, this.id)
           this.renderAdoptableDog(dog)
         })
       } else { // no adoptable dog(s) found
@@ -170,18 +171,22 @@ class Post {
 
   // Generating HTML for each adoptable dog and appending to adoptContainer
   renderAdoptableDog(dog) {
+    // get adoption container
     const adoptContainer = document.querySelector(`#adoption-container-${this.id}`)
+    // create dog media object
     const dogDiv = document.createElement("article")
     dogDiv.setAttribute("class", "media")
-    // media-left
+    // create media-left
     const figure = document.createElement("figure");
     figure.setAttribute("class", "media-left mt-4 mb-4")
-    // <p> for picture
+    // create picture <p>
     const picP = document.createElement("p")
     picP.setAttribute("class", "image is-96x96")
+    // create picture <img>
     const pic = document.createElement("img")
-    // if has primary photo
+    // set picture source
     if (dog.primary_photo_cropped != null) {
+      // if has primary photo
       pic.src = dog.primary_photo_cropped.medium
     } else {
       pic.src = "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/48637918/1/?bust=1596122132&width=450"
@@ -189,30 +194,32 @@ class Post {
     picP.appendChild(pic)
     figure.appendChild(picP)
     dogDiv.appendChild(figure)
-    // media-content
+    // create media-content
     const mediaContent = document.createElement("div")
     mediaContent.setAttribute("class", "media-content")
+    // create content
     const content = document.createElement("div")
     content.setAttribute("class", "content")
-    // <p> for name
+    // create name <p>
     const name = document.createElement("p")
     name.setAttribute("class", "has-text-danger is-size-5 mb-1")
     name.innerText = dog.name
     content.appendChild(name)
-    // <p> for details
+    // create details <p> - age, gender, and size
     const details = document.createElement("p")
     details.innerHTML = `${dog.age} ` + '<i class="fas fa-paw"></i>' + ` ${dog.gender} ` + '<i class="fas fa-paw"></i>' + ` ${dog.size}`
     content.appendChild(details)
-    // <p> for location
+    // create location <p> - city and state
     const location = document.createElement("p")
     location.innerHTML = '<i class="fas fa-map-marked-alt"></i>' + ` ${dog.contact.address.city}` + ', ' + `${dog.contact.address.state}`
     content.appendChild(location)
-    // <p> for url
+    // create url <p>
     const urlP = document.createElement("p")
     urlP.setAttribute("class", "has-text-link")
-    // <a> for url
+    // create url <a>
     const url = document.createElement("a")
     url.setAttribute("href", `${dog.url}`)
+    // open in new tab
     url.setAttribute("target", "_blank")
     url.innerText = `Tell Me More About ${dog.name}!`
     urlP.appendChild(url)
